60ddd0f144136b02133fc06f0ed591fddd8de541
cintia <cintiasilva7300@gmail.com>
2023-02-22 20:44:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+require('dotenv').config()
+const express = require('express');
+const mongoose = require('mongoose')
+const app = express();
+
+app.use (
+    express.urlencoded({
+        extended: true
+    })
+)
+
+app.use (express.json())
+
+//rotas
+const personRoutes = require('./routes/personRoutes')
+
+app.use('/person', personRoutes)
+
+const DB_USER = process.env.DB_USER;
+const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD) 
+
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.dfienfv.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log('Conectou com sucesso no mongoDb!')
+        app.listen(3002)
+    })
+    .catch((error) => console.log(error))    
+
+
+
---
+++ routes/personRoutes.js
@@ -0,0 +1,93 @@
+const router = require("express").Router();
+const Person = require("../models/person");
+
+router.post("/", async (req, res) => { //create
+  const { name, email, password, age } = req.body;
+
+  if (!name || !email || !password || !age) {
+    res.status(422).json({ error: "Preencha todos os campos!" });
+    return;
+  }
+
+  const person = {
+    name,
+    email,
+    password,
+    age,
+  };
+
+  try {
+    await Person.create(person);
+    res.status(201).json({ msg: "Cadastrado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.get("/", async (req, res) => {
+  //get
+  try {
+    const people = await Person.find();
+    res.status(200).json(people);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.get("/:id", async (req, res) => {//get id
+
+  const id = req.params.id; //estraindo o dado da requisiçao
+
+  try {
+    const person = await Person.findOne({ _id: id });
+
+    if (!person) {
+      res.status(422).json({ message: "Usuario não encontrada!" });
+      return;
+    }
+
+    res.status(200).json(person);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+router.patch("/:id", async (req, res) => {//update
+  const id = req.params.id;
+  const { name, email, password, age } = req.body;
+
+  const person = {
+    name,
+    email,
+    password,
+    age,
+  };
+
+  try {
+    const updatedPerson = await Person.updateOne({ _id: id }, person);
+
+    if (updatedPerson.matchedCount === 0) {
+      res.status(422).json({ msg: "Usuario não encontrada!" });
+      return;
+    }
+    res.status(200).json(person);
+  } catch (error) {}
+});
+
+router.delete("/:id", async (req, res) => { //delete
+  const id = req.params.id;
+
+  const person = await Person.findOne({ _id: id });
+  if (!person) {
+    res.status(422).json({ msg: "Usuario não encontrada!" });
+    return;
+  }
+  try {
+    await Person.deleteOne({ _id: id });
+    res.status(200).json({ msg: "Usuário deletado com sucesso!" });
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
+
+module.exports = router;

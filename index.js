d2f2b20945b9189410c1f683a3d4bc7d500efb87
cintia <cintiasilva7300@gmail.com>
2023-02-24 22:34:30 -0300

    ajuste realizado na inicialização do app

--- index.js
+++ index.js
@@ -24,15 +24,15 @@ const DB_USER = process.env.DB_USER;
 const PORT = process.env.PORT;
 const DB_PASSWORD = encodeURIComponent(process.env.DB_PASSWORD) 
 
-// mongoose
-//     .connect(
-//         `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.dfienfv.mongodb.net/?retryWrites=true&w=majority`
-//     )
-//     .then(() => {
-//         console.log('Conectou com sucesso no mongoDb!')
-//         app.listen(`${PORT}`)
-//     })
-//     .catch((error) => console.log(error))    
+mongoose
+    .connect(
+        `mongodb+srv://${DB_USER}:${DB_PASSWORD}@cluster0.dfienfv.mongodb.net/?retryWrites=true&w=majority`
+    )
+    .then(() => {
+        console.log('Conectou com sucesso no mongoDb!')
+        app.listen(`${PORT}`)
+    })
+    .catch((error) => console.log(error))    
 
 
 

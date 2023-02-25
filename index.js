393b0c6caecbc90aea7b4acb1e5a6ac3ed3e3dc9
cintia <cintiasilva7300@gmail.com>
2023-02-24 21:19:36 -0300

    ajuste realizado na inicialização do app

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 require('dotenv').config()
 const express = require('express');
 const mongoose = require('mongoose')
-
 const cors = require('cors')
-app.use(cors())
 
 const app = express();
+
+app.use(cors())
 
 app.use (
     express.urlencoded({
